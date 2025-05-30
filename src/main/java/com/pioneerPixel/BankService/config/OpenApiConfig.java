0744e3d601840674699fe3e14edc4a67eec3088f
Aleksandr Kozlov <vismut-888@yandex.ru>
2025-05-30 19:40:49 +0300

    Скорректировал Swagger для доступа по jwt токену

--- src/main/java/com/pioneerPixel/BankService/config/OpenApiConfig.java
+++ src/main/java/com/pioneerPixel/BankService/config/OpenApiConfig.java
@@ -1,12 +1,25 @@
 package com.pioneerPixel.BankService.config;
 
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "Bank API", version = "0.0.1"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
 
     @Bean
